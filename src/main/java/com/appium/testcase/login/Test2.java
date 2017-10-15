af5831b3c6a87d3639b1a0b99579c1c6f84f6a93
wangyuying <wangyuying@wondersgroup.com>
2017-10-16 06:27:53 +0800

    add3

---
+++ src/main/java/com/appium/testcase/login/Test2.java
@@ -0,0 +1,11 @@
+package com.appium.testcase.login;
+
+/**
+ * Created by wangyuying on 2017/10/16.
+ */
+public class Test2 {
+
+    public static void main(){
+        System.out.println("tijiao");
+    }
+}
